378127b3600e82ae4a243ba68a1f9871341a2c0f
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-04-01 15:17:16 -0300

    Applying data annotations and generating migrations

--- CatalogAPI/Models/Category.cs
+++ CatalogAPI/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CatalogAPI.Models;
 
@@ -8,9 +9,16 @@ public class Category
     {
         Products = new Collection<Product>();
     }
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? Description { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
     public ICollection<Product>? Products { get; set; }
 }
