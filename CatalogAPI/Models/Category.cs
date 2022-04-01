8441a61c500b354d75f96e46d4354e2f7e26dcb6
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-04-01 14:45:17 -0300

    Defining relationship between entities

--- CatalogAPI/Models/Category.cs
+++ CatalogAPI/Models/Category.cs
@@ -1,10 +1,17 @@
-﻿namespace CatalogAPI.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace CatalogAPI.Models;
 
 public class Category
 {
+    public Category()
+    {
+        Products = new Collection<Product>();
+    }
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public string? ImageUrl { get; set; }
+    public ICollection<Product>? Products { get; set; }
 }
 
